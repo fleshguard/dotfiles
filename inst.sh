105294768ee1cac82d462f5068fba2a9ca32b90e
fleshguard <168491129+fleshguard@users.noreply.github.com>
2024-05-18 13:12:30 -0400

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,3 +1,3 @@
 #Installs all of my packages for my daily driver :D
 
-sudo xbps-instal -Su && sudo xbps-install -S mesa-demos firefox tor mpv vlc nsxiv libreoffice wireshark evince gimp stellarium vim gcc clang distcc make zathura picom qemu virt-manager cargo rustup rust alsa-utils ffmpeg xfce4-screenshooter nasm xwallpaper kdenlive fetch xxd
+sudo xbps-install -Su && sudo xbps-install -S mesa-demos firefox tor mpv vlc nsxiv libreoffice wireshark evince gimp stellarium vim gcc clang distcc make zathura picom qemu virt-manager cargo rustup rust alsa-utils ffmpeg xfce4-screenshooter nasm xwallpaper kdenlive fetch xxd
