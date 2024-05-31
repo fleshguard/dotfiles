0b772207bb4ca3af968579a7ff0b7e83ed83e44c
fleshguard <168491129+fleshguard@users.noreply.github.com>
2024-05-31 18:43:00 -0400

    Delete inst.sh

--- inst.sh
+++
@@ -1,3 +0,0 @@
-# !! Around 5GB's in total | Daily driver binaries
-
-sudo xbps-install -Su && sudo xbps-install -S mesa-demos firefox tor mpv vlc nsxiv libreoffice wireshark evince gimp stellarium vim gcc clang distcc make zathura picom qemu virt-manager cargo rustup rust alsa-utils ffmpeg xfce4-screenshooter nasm xwallpaper kdenlive pfetch xxd
